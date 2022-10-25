435831370d9952c11a2758060ea5a015453e59ea
cgtie <cgtie.tese@gmail.com>
2022-10-25 10:37:18 -0500

    25oct2022 commit1

---
+++ src/controllers/unidades-eco.controller.js
@@ -0,0 +1,36 @@
+const googleUnidadesEco = require('../google-api/unidades-eco.google-api'); // goo = funciones de la API de Google
+
+// FUNCIONES ASINCRONAS A LA API DE GOOGLE
+
+async function getUnidadesEco(req,res) {
+    console.log("obteniendo");
+
+    const resultado = await googleUnidadesEco.sheetsAutomateGet();
+    //console.log(resultado);
+    res.send(resultado);
+    
+}
+
+async function postUnidadesEco(req,res) {
+    console.log("posteando");
+
+    const insertEjemplo = [[
+        "25/10/2022 10:10:59",
+        "Ejemplo_3",
+        "Ejemplo_3",
+        "Ejemplo_3",
+        "Ejemplo_3",
+        "030303",
+        "030303",
+        "Ejemplo_3",
+        "Ejemplo_3",
+        "Ejemplo_3",
+        "Ejemplo_3"
+      ]];
+    
+    const resultado = await googleUnidadesEco.sheetsAutomatePost(insertEjemplo);
+    //console.log(resultado);
+    res.send(resultado);
+}
+
+module.exports = { getUnidadesEco, postUnidadesEco};
