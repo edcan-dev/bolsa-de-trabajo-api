d761dcf436933ffe0b1240fd0590cf030b07cbf8
edcan-dev <israel.da.queenie@gmail.com>
2022-10-29 16:48:13 -0500

    29oct2022 cors-repair

--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-
-
+const cors = require('cors');
 
 const serverRouter = require('./routes/server.routes.js');
 const unidadesEcoRouter = require('./routes/unidades-eco.routes');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors);
 app.use(serverRouter);
 app.use(unidadesEcoRouter);
 
-
 app.listen(PORT, ()=>{console.log("Corriendo es 127.0.0.1:" + PORT)});
-
