55ae9c0f9aaf5ffb332defcd621ffede61611d39
edcan-dev <israel.da.queenie@gmail.com>
2022-10-29 20:28:51 -0500

    29oct2022 agregando-headers

--- src/server.js
+++ src/server.js
@@ -12,4 +12,16 @@ app.use(serverRouter);
 app.use(unidadesEcoRouter);
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.listen(PORT, ()=>{console.log("Corriendo es 127.0.0.1:" + PORT)});
+
+
+
+
